55d34aa32fafe0e318e50861b96542053b6421b6
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-06-06 11:57:09 -0300

    aula xp 42 libs - 06.06.22 - animaçoes e ui slide

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector(".inicio h1");
+
+titulo.onmouseover = ()=>{
+    titulo.classList.add("animate__animated", "animate__bounce");
+}
+
+// new Pageable(".container");
